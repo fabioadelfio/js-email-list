71f9c44f42a3573eb60e6eb1fd1bc88e05fda770
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-23 15:15:19 +0200

    bonus button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
+// Recupero l'ul in cui inserire le email
 const emailList = document.getElementById(`email-list`);
+
+// Salvo l'URL dell'API in una variabile
 const emailAPI = `https://flynn.boolean.careers/exercises/api/random/mail`;
 
-for (let i = 0; i < 10; i++) {
-    axios
-        .get(emailAPI)
-        .then(response => {
-            const email = response.data.response;
-            const listItem = document.createElement(`li`);
-
-            listItem.textContent = email;
-            emailList.append(listItem);
-        })
+function generateEmailList () {
+    // Creo un ciclo for per fare 10 richieste
+    for (let i = 0; i < 10; i++) {
+        // Uso Axios per fare una chiamata GET all'API
+        axios
+            .get(emailAPI)
+            .then(response => {
+                const email = response.data.response;
+    
+                // Creo un list item
+                const li = document.createElement(`li`);
+    
+                // Il list item che contiene l'email viene aggiunta all'email list
+                li.textContent = email;
+                emailList.append(li);
+            })
+    }
 }
+generateEmailList();
+
+const newEmailListBtn = document.getElementById(`new-email-list-btn`);
+
+newEmailListBtn.addEventListener(`click`, (e) => {
+    emailList.innerHTML = ``;
+
+    generateEmailList();
+})
