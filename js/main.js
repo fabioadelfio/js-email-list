4c6aa9a935fb760cec9911a6af079a4f92797ac0
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-23 14:56:10 +0200

    generate emails

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("JS OK");
+const emailList = document.getElementById(`email-list`);
+const emailAPI = `https://flynn.boolean.careers/exercises/api/random/mail`;
+
+for (let i = 0; i < 10; i++) {
+    axios
+        .get(emailAPI)
+        .then(response => {
+            const email = response.data.response;
+            const listItem = document.createElement(`li`);
+
+            listItem.textContent = email;
+            emailList.append(listItem);
+        })
+}
